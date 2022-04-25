b0166cf052fb201e534105ed1f026471484cbef5
Artyom Voitas <artyom.voitas@gmail.com>
2022-04-25 17:13:29 +0300

    fixed occured conflicts

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,12 +2,7 @@ const iconMenu = document.querySelector('.menu__icon');
 if (iconMenu) {
     const menuBody = document.querySelector('.menu__body');
     iconMenu.addEventListener('click', function(e) {
-<<<<<<< HEAD
         iconMenu.classList.toggle('active');
         menuBody.classList.toggle('active')
-=======
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active')
->>>>>>> 6be501dca0fa7b165ac878b2eb024deaebe265b1
     })
 }
