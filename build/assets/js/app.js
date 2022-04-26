d1fbf38c03ddb068a9fce4f1e54db2b7ef16a06e
Artyom Voitas <artyom.voitas@gmail.com>
2022-04-26 16:11:50 +0300

    added adaptive for intro section

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -6,3 +6,29 @@ if (iconMenu) {
         menuBody.classList.toggle('active')
     })
 }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'horizontal',
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            loop: true,
+            centeredSlides: true,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop: true,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+            loop: false,
+        },
+        1140: {
+            slidesPerView: 5,
+            loop: false,
+        }
+    },
+})
